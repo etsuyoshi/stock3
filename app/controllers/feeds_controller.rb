30135c40c9fab0b9057a60f5f933edbdbe0d0e50
tuyo1009 <tuyo1009@gmail.com>
2016-02-19 01:49:23 +0900

    ニュースフィード一覧画面の作成

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,6 +1,12 @@
 class FeedsController < ApplicationController
+  def index
+    # kaminariでpaging
+    @feeds = Feed.paginate(page: params[:page])
+    
+
+  end
   def show
     @feed = Feed.find(params[:id])
-    
+
   end
 end

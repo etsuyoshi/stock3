e2182318f67986a7263bd329f89a9df119fab04b
tuyo1009 <tuyo1009@gmail.com>
2016-03-20 15:28:14 +0900

    add 2 config assets to application

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 module S3
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -33,5 +31,6 @@ module S3
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end

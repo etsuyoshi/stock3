9d3eab755fb038bfd3cc95ea8c491f9d636b4b31
tuyo1009 <tuyo1009@gmail.com>
2016-05-31 22:57:49 +0900

    雇用統計

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -15,6 +15,11 @@ crumb :study_elementary do
   parent :study
 end
 
+crumb :why_you_invest do
+  link "なぜ投資をすべきか"
+  parent :study_elementary
+end
+
 crumb :study_aboutstock do
   link "株とは"
   parent :study_elementary
@@ -94,143 +99,148 @@ crumb :sell_matsui do
   parent :study_elementary
 end
 
+# 上級編
+crumb :study_advanced do
+  link "株価分析と売買手法"
+  parent :study
+end
+crumb :current_employment_statistics do
+  link "雇用統計と日本株"
+  parent :study_advanced
+end
+
 crumb :analytics do
   link "株式分析"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :apply_ipo do
   link "IPOに応募する"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :determine_policy do
   link "運用方針を決める"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :mini_kabu do
   link "ミニ株"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :analyst do
   link "アナリスト"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :analyst_report do
   link "アナリストレポート"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :about_po do
   link "公募とは"
-  parent :study
+  parent :study_advanced
 end
 crumb :apply_po do
   link "公募に応募する"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :bookbuilding do
   link "ブックビルディング"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :ipo_challenge do
   link "IPOチャレンジ"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :select_ipo do
   link "応募するIPO銘柄の選び方"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :success_ipo do
   link "IPOで成功する"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :methodology_find do
   link "銘柄を探す"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :survey_popular do
   link "人気株を探す"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :technical_chart do
   link "テクニカルチャート分析"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :shareholder_incentive do
   link "お得な株主優待"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :gyakuhibu do
   link "逆日歩"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :credit_deal do
   link "信用取引"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :mutual_fund do
   link "投資信託"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :noload do
   link "ノーロード投信"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :tax do
   link "税金"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :devidend do
   link "配当"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :nikkei225sakimono do
   link "日経225先物"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :trade_techinique do
   link "売買テクニック"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :foreign_investment do
   link "外国株投資"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :foreign_currency_deposits do
   link "外貨普通預金口座"
-  parent :study
+  parent :study_advanced
 end
 
 crumb :try_mini_kabu do
   link "ミニ株を始めてみよう"
-  parent :study
-end
-
-crumb :why_you_invest do
-  link "なぜ投資をすべきか"
-  parent :study
+  parent :study_advanced
 end
 
 
